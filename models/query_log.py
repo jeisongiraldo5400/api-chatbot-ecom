1af118550730c72975ef25e8a82cdce3c7cd5aed
Jeison <jeison5400@gmail.com>
2026-02-14 13:33:22 -0500

    feat: add user_id field to query log model with foreign key linking to users table

--- models/query_log.py
+++ models/query_log.py
@@ -8,11 +8,10 @@ from sqlalchemy import Column, ARRAY, String
 class QueryLog(SQLModel, table=True):
   __tablename__ = "queries_log"
   id: UUID = Field(default_factory=uuid4, primary_key=True)
+  user_id: Optional[UUID] = Field(default=None, foreign_key="users.id")
   service_id: int = Field(foreign_key="services.id")
   query_text: str
   answer_text: str
-
   context_chunks_ids: Optional[List[str]] = Field(default=None, sa_column=Column(ARRAY(String)))
-
   response_time: Optional[float] = None
   created_at: datetime = Field(default_factory=datetime.now)
